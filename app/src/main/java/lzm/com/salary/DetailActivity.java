1259e2e01d564a595949323a657eab770a796766
27617839 <27617839@qq.com>
2018-07-07 00:43:38 +0800

    third add by lzm 2018-7-7

--- app/src/main/java/lzm/com/salary/DetailActivity.java
+++ app/src/main/java/lzm/com/salary/DetailActivity.java
@@ -8,7 +8,7 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
-
+//lzm is author
 public class DetailActivity extends AppCompatActivity {
 
     private Button btn1;
